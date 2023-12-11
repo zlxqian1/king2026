015355773ff7b213b72d5fb921b6bac4cc84894f
zlxqian1 <101278285+zlxqian1@users.noreply.github.com>
2023-12-11 21:53:31 +0800

    Update chains.ts

--- src/config/chains.ts
+++ src/config/chains.ts
@@ -67,11 +67,33 @@ export const ethw = defineChain({
   },
 });
 
+export const kcc = defineChain({
+  id: 321,
+  name: "KCC Mainnet",
+  network: "KCC Mainnet",
+  nativeCurrency: {
+    decimals: 18,
+    name: "KCS",
+    symbol: "KCS",
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://kcc-rpc.com"],
+    },
+    public: {
+      http: ["https://kcc-rpc.com"],
+    },
+  },
+  blockExplorers: {
+    default: { name: "Explorer", url: "https://explorer.kcc.io/en" },
+  },
+});
+
 export const inscriptionChains = {
   eth: mainnet,
   bsc,
   opBNB,
-  okc,
+  // okc,
   polygon,
   fantom,
   avalanche,
@@ -80,6 +102,7 @@ export const inscriptionChains = {
   base,
   zkSync,
   classic,
+  kcc,
   coreDao,
   ethw,
   eos,
